418f798cfbb6df89f071c18e93998a3735c00a3b
Spita Alexandru <spita.alexandrum@gmail.com>
2026-05-15 00:42:41 +0300

    Modularized the project by implementing different directories with multiple CMake files

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #define F_CPU 16000000UL
-#include <avr/io.h>
-#include <util/delay.h>
+#define MACRO
+#include "../lib/init/init.hpp"
+
+
 
 int main(void) {
-    DDRB = (1 << PB1);
+    init();
 
     while(1) {
-        PORTB |= (1 << PB1);
-        _delay_ms(2000);
-        PORTB &= ~(1 << PB1);
-        _delay_ms(500);
+        if((PIND & ButtonPin )!= 0)
+        {
+            ChangeState(StartLED);
+        }
+        else
+        {
+            ChangeState(CloseLED);
+        }
     }
     return 0;
 }
